59310455126b49ee48ffc903bc016878319d51b7
Shane T. Fleming <t-shflem@microsoft.com>
2021-01-28 11:33:14 +0000

    Fixing an issue with file locations for the cli flow

--- arduino-cli/setup.sh
+++ arduino-cli/setup.sh
@@ -8,8 +8,31 @@ tar -xvf arduino-cli_nightly-latest_Linux_64bit.tar.gz
 
 # copy over the config file into the users home directory
 mkdir -p ~/.arduino15/
+
+# Generate a YAML file with the ESP32 board stuff
+echo -e "board_manager:" > arduino-cli.yaml
+echo -e "    additional_urls: [https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json]" >> arduino-cli.yaml
+echo -e "daemon:" >> arduino-cli.yaml
+echo -e "    port: \"50051\"" >> arduino-cli.yaml
+echo -e "directories:" >> arduino-cli.yaml
+echo -e "    data: $HOME/.arduino15" >> arduino-cli.yaml
+echo -e "    downloads: $HOME/.arduino15/staging" >> arduino-cli.yaml
+echo -e "    user: $HOME/Arduino" >> arduino-cli.yaml
+echo -e "library:" >> arduino-cli.yaml
+echo -e "    enable_unsafe_install: false" >> arduino-cli.yaml
+echo -e "logging:" >> arduino-cli.yaml
+echo -e "    file: \"\"" >> arduino-cli.yaml
+echo -e "    format: text" >> arduino-cli.yaml
+echo -e "    level: info" >> arduino-cli.yaml
+echo -e "metrics:" >> arduino-cli.yaml
+echo -e "    addr: 9090" >> arduino-cli.yaml
+echo -e "    enabled: true" >> arduino-cli.yaml
+echo -e "sketch:" >> arduino-cli.yaml
+echo -e "    always_export_binaries: false" >> arduino-cli.yaml
+
 cp arduino-cli.yaml ~/.arduino15/arduino-cli.yaml 
 ./arduino-cli core update-index
+./arduino-cli core install arduino:avr
 ./arduino-cli core install esp32:esp32
 
 ./arduino-cli compile --fqbn esp32:esp32:tinypico Blink
