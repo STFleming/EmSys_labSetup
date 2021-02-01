65ac73f71edf0396e3d46b8ca5e6c0ebc2b6f7d2
Shane Fleming <shane.t.fleming@gmail.com>
2021-02-01 19:52:45 +0000

    Changing the setup to also build arduino-cli

--- setup.sh
+++ setup.sh
@@ -12,18 +12,8 @@ tar -xvf ./arduino-${VERSION}-linux64.tar.xz
 # Adding an alias to open the arduino IDE
 echo "alias arduino=${DIR}/arduino-${VERSION}/arduino" >> ~/.bashrc
 
-# Getting PulseView  
-wget https://sigrok.org/download/binary/pulseview/PulseView-0.4.2-i386.AppImage
-chmod +x PulseView-0.4.2-i386.AppImage
-echo "alias PulseView=${DIR}/PulseView-0.4.2-i386.AppImage" >> ~/.bashrc
-
-pip install pyserial
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
-
-wget https://sigrok.org/download/binary/sigrok-firmware-fx2lafw/sigrok-firmware-fx2lafw-bin-0.1.7.tar.gz
-tar -xvf sigrok-firmware-fx2lafw-bin-0.1.7.tar.gz
-mkdir -p ~/.local/share/sigrok-firmware/
-cp sigrok-firmware-fx2lafw-bin-0.1.7/* ~/.local/share/sigrok-firmware
+cd ./arduino-cli
+./setup.sh
 
 echo "-----------------------------------------------------"
 echo " Finished! Please log out and log back in again"
