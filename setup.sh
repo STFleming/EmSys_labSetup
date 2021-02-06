aa4913f43a4d6a2ae22e20b465d97222643978c9
Shane T. Fleming <t-shflem@microsoft.com>
2021-02-06 23:23:25 +0000

    Improving the setup curl script

--- setup.sh
+++ setup.sh
@@ -8,8 +8,6 @@ rm -rf ./EmSys_LabSetup
 # Make the EmSys directory
 mkdir -p ~/EmSys/
 
-# 
-
 # Download arduino-cli
 wget https://downloads.arduino.cc/arduino-cli/nightly/arduino-cli_nightly-latest_Linux_64bit.tar.gz
 mv arduino-cli_nightly-latest_Linux_64bit.tar.gz ~/EmSys/
@@ -22,15 +20,15 @@ tar -xvf arduino-cli_nightly-latest_Linux_64bit.tar.gz
 mkdir -p ~/EmSys/Blink
 
 echo -e "const int pin = 14;" > ~/EmSys/Blink/Blink.ino
-echo -e "void setup() {" > ~/EmSys/Blink/Blink.ino
-echo -e " pinMode(pin, OUTPUT); " > ~/EmSys/Blink/Blink.ino
-echo -e "} " > ~/EmSys/Blink/Blink.ino
-echo -e "void loop() {" > ~/EmSys/Blink/Blink.ino
-echo -e " digitalWrite(pin, HIGH);" > ~/EmSys/Blink/Blink.ino
-echo -e " delay(1000);" > ~/EmSys/Blink/Blink.ino
-echo -e " digitalWrite(pin, LOW);" > ~/EmSys/Blink/Blink.ino
-echo -e " delay(1000);" > ~/EmSys/Blink/Blink.ino
-echo -e "}" > ~/EmSys/Blink/Blink.ino
+echo -e "void setup() {" >> ~/EmSys/Blink/Blink.ino
+echo -e " pinMode(pin, OUTPUT); " >> ~/EmSys/Blink/Blink.ino
+echo -e "} " >> ~/EmSys/Blink/Blink.ino
+echo -e "void loop() {" >> ~/EmSys/Blink/Blink.ino
+echo -e " digitalWrite(pin, HIGH);" >> ~/EmSys/Blink/Blink.ino
+echo -e " delay(1000);" >> ~/EmSys/Blink/Blink.ino
+echo -e " digitalWrite(pin, LOW);" >> ~/EmSys/Blink/Blink.ino
+echo -e " delay(1000);" >> ~/EmSys/Blink/Blink.ino
+echo -e "}" >> ~/EmSys/Blink/Blink.ino
 
 # copy over the config file into the users home directory
 mkdir -p ~/.arduino15/
@@ -62,3 +60,8 @@ cp arduino-cli.yaml ~/.arduino15/arduino-cli.yaml
 
 ./arduino-cli compile --fqbn esp32:esp32:tinypico Blink
 ./arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:tinypico Blink 
+
+# Append commands to the bashrc
+echo -e "alias emsys_compile="~/EmSys/arduino-cli upload" >> ~/.bashrc
+echo -e "alias emsys_upload="~/EmSys/arduino-cli upload" >> ~/.bashrc
+
