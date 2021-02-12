b946b65c95ed647ec39ce9d64a740dd8679f6d95
Shane T. Fleming <t-shflem@microsoft.com>
2021-02-12 09:15:26 +0000

    Added some text to the cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,9 @@
 # this can be updated and it will effect all users... so use with care
 # author: stf
 
+echo "Cleaning up..."
 rm -rf ~/EmSys_labSetup
 rm -rf ~/EmSys/arduino-cli_nightly-latest_Linux_64bit.tar.gz
 rm -rf ~/.local/share/Trash/files/*
 rm -rf ~/.cache/*
+echo "Cleanup complete"
