10cbda13b341f980fd342c96727f2dcff60ed35e
Shane T. Fleming <t-shflem@microsoft.com>
2021-03-01 22:49:33 +0000

    It should also clean the old file a bit better

--- cleanup.sh
+++ cleanup.sh
@@ -16,5 +16,6 @@ kill -9 $(ps aux | awk '/ttyUSB0/ && ! /arduino-cli/ {print $2}') &> /dev/null
 
 
 echo "getting latest LetESP32.h..."
+rm -rf ~/Arduino/libraries/LetESP32/LetESP32.h
 mkdir -p ~/Arduino/libraries/LetESP32
 wget https://raw.githubusercontent.com/STFleming/EmSys_Lab2/main/let-esp32/LetESP32.h -P ~/Arduino/libraries/LetESP32/
